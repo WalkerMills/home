cc9f5341c3ff07e4941243cae2ce7c42441b1f9e
Walker Mills <walker.mills7@gmail.com>
2014-02-23 08:59:11 -0800

    Adds Leonardo number type/class
    Adds template code to make the sorting truly generic for both arrays and
    STL containers (or any class with iterator, reverse_iterator, and []
    methods defined). And in the global scope of the header, I defined a new
    class to represent the set of Leonardo numbers, with the ++, --, and ~
    (previous) operators and conversion to unsigned defined.
    
    Implements all template boilerplate
    
    The template mechanics for easily handling arrays and STL containers are
    fully implemented; smoothsort isn't implemented yet, but insertion sort,
    quicksort, and introsort are.

--- src/c++/sort.hh
+++ src/c++/sort.hh
@@ -3,33 +3,93 @@
 
 #include <vector>
 
-
 namespace sort {
+    template<typename T>
+    void sort::insertion_sort(T *input, unsigned start, unsigned stop);
+    template<typename T, typename C=std::vector>
+    void sort::insertion_sort(C<T> input, unsigned start, unsigned stop);
+    template<typename T>
+    void sort::insertion_sort(T *input, unsigned size);
+    template<typename T, typename C=std::vector>
+    void sort::insertion_sort(C<T> input);
 
     template<typename T>
-    void insertion_sort(std::vector<T> input, int start, int stop);
+    void sort::smoothsort(T *input, unsigned start, unsigned stop);
+    template<typename T, typename C=std::vector>
+    void sort::smoothsort(C<T> input, unsigned start, unsigned stop);
+    template<typename T>
+    void sort::smoothsort(T *input, unsigned size);
+    template<typename T, typename C=std::vector>
+    void sort::smoothsort(C<T> input);
 
     template<typename T>
-    void insertion_sort(std::vector<T> input);
+    void sort::quicksort(T *input, unsigned start, unsigned stop);
+    template<typename T, typename C=std::vector>
+    void sort::quicksort(C<T> input, unsigned start, unsigned stop);
+    template<typename T>
+    void sort::quicksort(T *input, unsigned size);
+    template<typename T, typename C=std::vector>
+    void sort::quicksort(C<T> input);
 
     template<typename T>
-    void heapsort(std::vector<T> input, int start, int stop);
-
+    void sort::introsort(T *input, unsigned start, unsigned stop, 
+                         unsigned depth);
+    template<typename T, typename C=std::vector>
+    void sort::introsort(C<T> input, unsigned start, unsigned stop, 
+                         unsigned depth);
     template<typename T>
-    void heapsort(std::vector<T> input);
-
-    template<typename T>
-    int partition(std::vector<T> input, int start, int stop);
-
-    template<typename T>
-    void quicksort(std::vector<T> input, int start, int stop);
-
-    template<typename T>
-    void introsort(std::vector<T> input, int start, int stop, int depth);
-
-    template<typename T>
-    void introsort(std::vector<T> input);
-
+    void sort::introsort(T *input, unsigned size);
+    template<typename T, typename C=std::vector>
+    void sort::introsort(C<T> input);
 }
+
+class leonardo {
+private:
+    // Current Leonardo number
+    unsigned a;
+    // Previous Leonardo number
+    unsigned b;
+
+public:
+    // Default constructor
+    leonardo() throw() : a(1), b(1) { 
+        return; 
+    }
+    // Copy constructor
+    leonardo(const leonardo &l) throw() : a(l.a), b(l.b) { 
+        return; 
+    }
+
+    // Calculate the gap between the current and previous Leonardo numbers
+    unsigned gap() const throw() {
+        return a - b;
+    }
+
+    // Increment to the next Leonardo number
+    leonardo &operator++() throw() {
+        unsigned tmp = a;
+        
+        a = a + b + 1;
+        b = tmp;
+    }
+
+    // Decrement to the previous Leonardo number
+    leonardo &operator--() throw() {
+        unsigned tmp = b;
+        
+        b = a - b - 1;
+        a = tmp;
+    }
+
+    // Return current value
+    operator unsigned() const throw() {
+        return a;
+    }
+
+    // Return previous value
+    unsigned operator~() const throw() {
+        return b;
+    }
+};
 
 #endif // __SORT_HH__
