5ace34b9d8e14b8d9e5a00016d74804d27bd4336
Walker Mills <walker.mills7@gmail.com>
2016-01-14 13:03:29 -0800

    Untrack src

--- src/cython/hello.pyx
+++
@@ -1,5 +0,0 @@
-import sys
-
-def hello():
-    sys.stdout.write("Hello World!\n")
-    sys.stdout.flush()
--- src/cython/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(name = 'Hello World',
-      ext_modules = cythonize("*.pyx"))
